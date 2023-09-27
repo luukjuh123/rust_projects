415fb436a5ffd69a676ec709f387a068ab27a571
luuk <luuk.oude-grotebevelsborg@asr.nl>
2023-09-27 15:12:54 +0000

    latest

--- web/api/invoicedirect/src/handlers/init_db.rs
+++ web/api/invoicedirect/src/handlers/init_db.rs
@@ -1,7 +1,11 @@
 use crate::db::conn::DB_CONN;
 
 #[tokio::main]
-pub async fn init_db() -> Result<()> {
-    let conn = DB_CONN.lock()?;
-    conn.execute_batch(r"CREATE TABLE IF NOT EXISTS customer (id INTEGER PRIMARY KEY, name TEXT, email TEXT);")
+pub async fn init_db() -> Result<(), duckdb::Error> {
+    let lock = DB_CONN.lock().map_err(|_| Error("Mutex is poisoned"))?;
+    let conn = &*lock;
+    conn.execute_batch(r"CREATE TABLE IF NOT EXISTS customer (id INTEGER PRIMARY_KEY, name TEXT, email TEXT)")
+        .map_err(|e| e);
+
+    Ok(())
 }
